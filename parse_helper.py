255efec915899ad1f84bbcd80c068e1c2cd7125c
kwlou <klou449@gmail.com>
2020-02-07 14:45:37 -0500

    small helper function to parse through files

--- parse_helper.py
+++ parse_helper.py
@@ -2,14 +2,15 @@
 from glob import glob
 import os
 from subprocess import call
+from fnmatch import fnmatch
 
 
-basedir = '/qtim2/users/data/BAV/ANALYSIS/COREGISTRATION/'
+def files_to_list(basedir,files_of_interest):
+    '''
+    expects a basedir and files_of_interest as strings, returns generator object containing the directory of the files of interest
+    '''
+    for root, dirs, files in os.walk(basedir):
+        for name in files:
+            if fnmatch(name,files_of_interest):
+                yield os.path.join(root, name)
 
-for root, dirs, files in os.walk(basedir):
-   for name in files:
-      print(os.path.join(root, name))
-   for name in dirs:
-      print(os.path.join(root, name))
-
-      #generator type object with __iter__() and __next__() methods

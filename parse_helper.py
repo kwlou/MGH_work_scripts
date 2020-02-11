9c6b4a0099cd9f3575a06c8a4c45f7bfdb1e0197
kwlou <klou449@gmail.com>
2020-02-11 09:55:40 -0500

    small update, added functionality to ignore directories containing user input "except_case"

--- parse_helper.py
+++ parse_helper.py
@@ -5,12 +5,12 @@ from subprocess import call
 from fnmatch import fnmatch
 
 
-def files_to_list(basedir,files_of_interest):
+def files_to_list(basedir,files_of_interest,except_case=None):
     '''
     expects a basedir and files_of_interest as strings, returns generator object containing the directory of the files of interest
     '''
     for root, dirs, files in os.walk(basedir):
         for name in files:
-            if fnmatch(name,files_of_interest):
+            if fnmatch(name,files_of_interest) and any([fnmatch(except_case,i) for i in files]):
                 yield os.path.join(root, name)
 

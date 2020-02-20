580da2d61fc0bfd9d29e0f0e088eb953fdeb16c2
kwlou <klou449@gmail.com>
2020-02-20 13:31:56 -0500

    added clause to ignore except_case if not provided

--- parse_helper.py
+++ parse_helper.py
@@ -4,13 +4,17 @@ import os
 from subprocess import call
 from fnmatch import fnmatch
 
-
+#TODO add list functionality to this
 def files_to_list(basedir,files_of_interest,except_case=None):
     '''
     expects a basedir and files_of_interest as strings, returns generator object containing the directory of the files of interest
     '''
     for root, dirs, files in os.walk(basedir):
         for name in files:
-            if fnmatch(name,files_of_interest) and any([fnmatch(except_case,i) for i in files]):
-                yield os.path.join(root, name)
+            if except_case is not None:
+                if fnmatch(name,files_of_interest) and not any([fnmatch(i,except_case) for i in files]):
+                    yield os.path.join(root, name)
+            else:
+                if fnmatch(name,files_of_interest):
+                    yield os.path.join(root, name)
 

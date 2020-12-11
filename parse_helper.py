b709b9d90d8079f2e384dc345db80f828b45bb45
Kevin W Lou <kwl16@bryce.nmr.mgh.harvard.edu>
2020-12-11 14:57:21 -0500

    added list functionality to substring matching for files finding

--- parse_helper.py
+++ parse_helper.py
@@ -10,14 +10,17 @@ def files_to_list(basedir,files_of_interest,except_case=None):
     '''
     expects a basedir and files_of_interest as strings, returns generator object containing the directory of the files of interest
     '''
+    if type(files_of_interest) == str:
+        files_of_interest = [files_of_interest]
     for root, dirs, files in os.walk(basedir):
         for name in files:
-            if except_case is not None:
-                if fnmatch(name,files_of_interest) and not any([fnmatch(i,except_case) for i in files]):
-                    yield os.path.join(root, name)
-            else:
-                if fnmatch(name,files_of_interest):
-                    yield os.path.join(root, name)
+            for file_string in files_of_interest:
+                if except_case is not None:
+                    if fnmatch(name,file_string) and not any([fnmatch(i,except_case) for i in files]):
+                        yield os.path.join(root, name)
+                else:
+                    if fnmatch(name,file_string):
+                        yield os.path.join(root, name)
 
 def files_by_date(filelist):
     return sorted(filelist,key=os.path.getctime)

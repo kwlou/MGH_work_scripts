fdd698f5097f1f27ab8f94ca1c014a7d1c52d3c8
Kevin W Lou <kwl16@bryce.nmr.mgh.harvard.edu>
2021-03-24 10:11:37 -0400

    dummy commit here

--- parse_helper.py
+++ parse_helper.py
@@ -22,5 +22,21 @@ def files_to_list(basedir,files_of_interest,except_case=None):
                     if fnmatch(name,file_string):
                         yield os.path.join(root, name)
 
+def folders_to_list(basedir,files_of_interest,except_case=None):
+    '''
+    expects a basedir and files_of_interest as strings, returns generator object containing the directory of the files of interest
+    '''
+    if type(files_of_interest) == str:
+        files_of_interest = [files_of_interest]
+    for root, dirs, files in os.walk(basedir):
+        for name in dirs:
+            for file_string in files_of_interest:
+                if except_case is not None:
+                    if fnmatch(name,file_string) and not any([fnmatch(i,except_case) for i in files]):
+                        yield os.path.join(root, name)
+                else:
+                    if fnmatch(name,file_string):
+                        yield os.path.join(root, name)
+
 def files_by_date(filelist):
     return sorted(filelist,key=os.path.getctime)
